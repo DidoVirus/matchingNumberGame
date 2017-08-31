d9ea6a40955ab5ab70cac9a110028c6864ad3a5f
dido virus <kamal.alaya@outlook.com>
2017-08-31 17:46:45 +0100

    improve the function toshuffle the array

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// var indeie = [1,2,3,4,5,6];
 var n = 10;
 var values = [];
 
@@ -16,15 +15,18 @@ state.push("F")
 }
 
 // shuffle the array
-for(g=0; g<n*2; g++){
-var r_index1 = Math.round(Math.random() * ((n * 2) - 1));
-var r_index2 = Math.round(Math.random() * ((n * 2) - 1));
-
-var c ;
-c = values[r_index1];
-values[r_index1] = values[r_index2];
-values[r_index2] = c;
-};
+values.sort(function() {
+  return Math.random() - Math.random();
+});
+// for(g=0; g<n*2; g++){
+// var r_index1 = Math.round(Math.random() * ((n * 2) - 1));
+// var r_index2 = Math.round(Math.random() * ((n * 2) - 1));
+//
+// var c ;
+// c = values[r_index1];
+// values[r_index1] = values[r_index2];
+// values[r_index2] = c;
+// };
 
 // Draw the elements
 var bigBox = document.getElementById("game");
@@ -46,14 +48,15 @@ for (x = 0; x < values.length; x++) {
 
 
   var littelBox = document.createElement("div");
-  var value = document.createTextNode(element);
-  littelBox.appendChild(value);
-  bigBox.appendChild(littelBox);
-  littelBox.setAttribute("id", x+1)
-  littelBox.addEventListener("click", function (e) {
-    console.log(e.target.id);
-     state[x] = "T";
-     bigBox.style.display = 'none';
-     bigBox.style.display = 'block';
+var value = document.createTextNode(element);
+littelBox.appendChild(value);
+bigBox.appendChild(littelBox);
+littelBox.setAttribute("id", x + 1)
+littelBox.addEventListener("click", function(e) {
+      console.log(e.target.id);
+      state[x] = "T";
+       e.target.textContent = 'Potato'
+      // bigBox.style.display = 'none';
+      // bigBox.style.display = 'block';
   });
 }
